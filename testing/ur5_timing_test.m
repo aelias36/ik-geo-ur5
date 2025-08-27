d0ff6c050344b66abfcff41a474286a223841acc
Alex Elias <aelias36@yahoo.com>
2025-08-27 16:11:17 -0400

    timing/accuracy tests

--- testing/ur5_timing_test.m
+++ testing/ur5_timing_test.m
@@ -33,6 +33,7 @@ Q = hardcoded_ur5_ik_mex(R_06, p_0T);
 disp(rad2deg(Q))
 
 %% Generate 10,000 random test cases
+rng("default") % Reset random number generator
 N = 10e3;
 R_list = NaN(3,3,N);
 p_list = NaN(3,N);
@@ -46,6 +47,7 @@ end
 %% Generate mex code for timing test
 
 % Generate fake list with duplicate R, p but the right size
+N = 1000;
 R_list_codegen = NaN(3,3,N);
 p_list_codegen = NaN(3,N);
 for i = 1:N
@@ -55,59 +57,120 @@ end
 
 codegen -report ur5_timing_test_inner.m -args {R_list_codegen, p_list_codegen}
 
+
 %% Conduct timing test
 N_outer = 1000;
+N = 1000;
 T_list = NaN([N_outer 1]);
 for i = 1:N_outer
- [T_list(i), Q_list] = ur5_timing_test_inner_mex(R_list, p_list);
+ [T_list(i), Q_list] = ur5_timing_test_inner_mex(R_list(:,:,1:N), p_list(:,1:N));
 end
 plot(sort(T_list)*1e6, '-x') 
-min(T_list)*1e6
- %(microseconds)
+min(T_list)*1e6 % microseconds
 
 %%
 for i = 1:10
-    Q_i = hardcoded_ur5_ik_mex(R_list(:,:,i), p_list(:,i));
+    % Q_i = hardcoded_ur5_ik_mex(R_list(:,:,i), p_list(:,i));
+    Q_i = hardcoded_ur5_IK_one_solution(R_list(:,:,i), p_list(:,i));
 end
 
 %% Accuracy test
+rng("default")
 N = 10e3;
+% N = 1000;
 q_norms = NaN([N 1]);
 p_norms = NaN([N 1]);
 R_norms = NaN([N 1]);
 for i = 1:N
     q = rand_angle([6 1]);
     [R_06, p_0T] = fwdkin(kin, q);
-    Q = hardcoded_ur5_ik_mex(R_06, p_0T);
+    Q = hardcoded_ur5_IK_mex(R_06, p_0T);
+    % Q = reshape(Q_list(:,i), 6, 8);
     [q_test, ~, diff_norm] = closest_q(Q, q);
     q_norms(i) = diff_norm;
     [R_06_test, p_0T_test] = fwdkin(kin, q_test);
     p_norms(i) = norm(p_0T - p_0T_test);
     
     % R_norms(i) = real(acos((trace(R_06'*R_06_test) - 1) / 2));
-    R = R_06'*R_06_test;                          % relative rotation
-    s = 0.5 * [R(3,2)-R(2,3); R(1,3)-R(3,1); R(2,1)-R(1,2)];  % = sinθ * u
-    c = 0.5 * (trace(R) - 1);              % = cosθ
+    R = R_06'*R_06_test;
+    s = 0.5 * [R(3,2)-R(2,3); R(1,3)-R(3,1); R(2,1)-R(1,2)];
+    c = 0.5 * (trace(R) - 1);
     sn = norm(s);
     
     % Primary formula
     R_norms(i) = atan2(sn, c);
 end
 
-%% Error in q
-semilogy(sort(q_norms), linewidth=2);
-title("Joint-space Error")
-xlabel("Solution # (Sorted)")
-ylabel("||\Delta q|| (Rad)")
+%% Error medians
+clc
+median(q_norms)
+median(R_norms)
+median(p_norms)
 
-%% Error in p_0T
-semilogy(sort(p_norms), linewidth=2);
-title("Task-Space Error (Position)")
-xlabel("Solution # (Sorted)")
-ylabel("||\Delta p_{0T}|| (m)")
+
+%% Error in q
+% Export with 600 dpi
+
+semilogy(sort(q_norms), '.k');
+% title("Joint-space Error")
+xlabel("Solution \# (Sorted)", Interpreter="latex")
+ylabel("$|\!|\Delta q|\!|$ (rad)", Interpreter="latex")
+
+fontsize(12, 'points')
+xaxisproperties= get(gca, 'XAxis');
+xaxisproperties.TickLabelInterpreter = 'latex';
+yaxisproperties= get(gca, 'YAxis');
+yaxisproperties.TickLabelInterpreter = 'latex';
 
 %% Error in R_06
-semilogy(sort(R_norms), linewidth=2);
-title("Task-Space Error (Rotation)")
-xlabel("Solution # (Sorted)")
-ylabel("\angle \Delta R_{06} (rad)")
+semilogy(sort(R_norms), '.k');
+% title("Task-Space Error (Rotation)")
+xlabel("Solution \# (Sorted)", Interpreter="latex")
+ylabel("$\angle(\Delta R_{06})$ (rad)", Interpreter="latex")
+
+fontsize(12, 'points')
+xaxisproperties= get(gca, 'XAxis');
+xaxisproperties.TickLabelInterpreter = 'latex';
+yaxisproperties= get(gca, 'YAxis');
+yaxisproperties.TickLabelInterpreter = 'latex';
+
+%% Error in p_0T
+semilogy(sort(p_norms), '.k');
+% title("Task-Space Error (Position)")
+xlabel("Solution \# (Sorted)", Interpreter="latex")
+ylabel("$|\!|\Delta p_{0T}|\!|$ (m)", Interpreter="latex")
+
+fontsize(12, 'points')
+xaxisproperties= get(gca, 'XAxis');
+xaxisproperties.TickLabelInterpreter = 'latex';
+yaxisproperties= get(gca, 'YAxis');
+yaxisproperties.TickLabelInterpreter = 'latex';
+
+%% Show all 3 on one graph
+semilogy(sort(q_norms), '.k'); hold on
+semilogy(sort(R_norms), '.', color=diagrams.colors.red);
+semilogy(sort(p_norms), '.', color=diagrams.colors.blue);
+ hold off
+% title("Joint-space Error")
+xlabel("Solution \# (Individually sorted)", Interpreter="latex")
+ylabel("Solution Error", Interpreter="latex")
+
+fontsize(12, 'points')
+xaxisproperties= get(gca, 'XAxis');
+xaxisproperties.TickLabelInterpreter = 'latex';
+yaxisproperties= get(gca, 'YAxis');
+yaxisproperties.TickLabelInterpreter = 'latex';
+
+[~, objh] = legend(["$|\!|\Delta q|\!|$ (rad)", "$\angle(\Delta R_{06})$ (rad)", "$|\!|\Delta p_{0T}|\!|$ (m)"], ...
+    Interpreter="latex", Location="northwest");
+objhl = findobj(objh, 'type', 'line');
+set(objhl, 'LineStyle', '-', 'Marker','none', 'LineWidth',2);
+%% Test angle calculation is correct
+
+R = rot(rand_normal_vec, 2.123e-8);
+s = 0.5 * [R(3,2)-R(2,3); R(1,3)-R(3,1); R(2,1)-R(1,2)];
+c = 0.5 * (trace(R) - 1);
+sn = norm(s);
+
+atan2(sn, c) % atan2 is accurate
+real(acos((trace(R) - 1) / 2)) % acos is NOT accurate
--- testing/ur5_timing_test_inner.m
+++ testing/ur5_timing_test_inner.m
@@ -1,8 +1,10 @@
 %% This function gets converted to MEX code to conduct the timing test
 
 function [T, Q_list] = ur5_timing_test_inner(R_list, p_list)
-N = 1e3;
-Q_list = NaN(6, 8, N);
+N = 1000;
+Q_list = NaN(6*8, N);
+
+% Q_list = NaN(6, N);
 
 % Timer starts here
 tic;
@@ -11,7 +13,10 @@ for i = 1:N
     R_i = R_list(:,:,i);
     p_i = p_list(:,i);
     Q_i = hardcoded_ur5_IK(R_i, p_i);
-    Q_list(:,1:width(Q_i),i) = Q_i;
+    Q_list(1:numel(Q_i),i) = Q_i(:);
+
+    % Q_list(:,i) = hardcoded_ur5_IK_one_solution(R_i, p_i);
+
 end
 % Timer ends here
 T_total = toc;
