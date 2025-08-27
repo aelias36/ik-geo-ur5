c72aa6e2eba7cc3a5b231bff735de168ee59cf7d
Alex Elias <aelias36@yahoo.com>
2025-08-27 16:44:33 -0400

    Update ur5_singular_solution_demo.m

--- testing/ur5_singular_solution_demo.m
+++ testing/ur5_singular_solution_demo.m
@@ -1,5 +1,3 @@
-% Generate singular pose
-
 zv = [0;0;0];
 ex = [1;0;0];
 ey = [0;1;0];
@@ -9,12 +7,13 @@ kin.P = [0.1625*ez zv -0.425*ex -0.3922*ex -0.1333*ey-0.0997*ez zv -0.0996*ey];
 kin.H = [ez -ey -ey -ey -ez -ey];
 kin.joint_type = zeros([1 6]);
 
+% Generate singular pose
 q_0 = zeros([6 1]);
 [R_06, p_0T] = fwdkin(kin, q_0);
 
-%% Confirm singularity
+% Confirm singularity
 J = robotjacobian(kin, q_0);
 disp("Min singular value: " + min(svd(J)))
 
-%% IK still works at singularity
+% IK still works at singularity
 [Q, is_LS] = hardcoded_ur5_IK(R_06, p_0T)
